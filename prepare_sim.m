e5ae774120877dc0e299647e4709e19a8efcf610
HP <HP@DESKTOP-LAPUETE>
2025-03-27 11:12:42 +0100

    Feat: Added the latest 'Enhanced_Ph_modelmat_2025_03_26.mat' from newtork trainig

--- prepare_sim.m
+++ prepare_sim.m
@@ -2,12 +2,13 @@ global pH_const
 
 load('training_data');
 load('training_data_simple');
-load('enhanced_ph_model.mat')
+load('Enhanced_Ph_Modelmat_2025_03_26.mat');
 
 pH_const = struct();
 pH_const.ts = 5;
 pH_const.net = pH_net;
-pH_const.net_simple = pH_net_simple;
+pH_const.net_simple = ph_predictor;
+% pH_const.net_simple = pH_net_simple;
 [x_0, u_0, d_0, y_0] = get_init_op();
 
 % Try to sync network internal state. May not produce the desired result if network model is inaccurate  
@@ -18,4 +19,4 @@ pH_const.u_0 = u_0; pH_const.x_0 = x_0; pH_const.d_0 = d_0; pH_const.y_0 = y_0;
 pH_sim_data = sim("pH_control_loops");
 
 % Uncomment or paste in command window to save simulation data
-% save("simulation_data", "pH_sim_data");
+save("simulation_data", "pH_sim_data");
